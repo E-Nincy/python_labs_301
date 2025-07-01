f972af99ad379a4b86b4b379196583a27ff39bb9
E-Nincy <escotonincy10@outlook.es>
2025-07-01 14:29:53 +0800

    add solution

--- 02_classes-objects-methods/02_01_empty_form.py
+++ 02_classes-objects-methods/02_01_empty_form.py
@@ -10,3 +10,25 @@
 #
 # Model such an application form as a Python class below, and instantiate
 # a few objects from it.
+
+class MedicalForm:
+    def __init__(self, name, age, gender , symptoms, allergies):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.symptoms = symptoms
+        self.allergies = allergies
+
+    def __str__(self):
+        return (f"Patient: {self.name}, Age: {self.age}, Gender: {self.gender}, "
+                f"Symptoms: {self.symptoms}, Allergies: {self.allergies}")
+    
+# Create form (objects)
+patient1 = MedicalForm("Ana Garcia", 28, "Female", "Headache, fever", "None")
+patient2 = MedicalForm("Simon Baumhauer", 29, "Male", "Back pain", "Ibuprofen")
+patient3 = MedicalForm("Roxana Romero", 35, "Female", "Fatigue, cough", "Dust")
+
+# Prit form and Object
+print(patient1)
+print(patient2)
+print(patient3)
