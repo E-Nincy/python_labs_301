c215abeabbba21af7e1a91dc2be5760578204cec
E-Nincy <escotonincy10@outlook.es>
2025-07-10 15:24:51 +0800

    fix text

--- 05_exceptions/05_05_check_files.py
+++ 05_exceptions/05_05_check_files.py
@@ -4,16 +4,18 @@
 # with specific `except` statements, and continue to do the calculation
 # only if neither of them applies.
 
-file_name = 'integers.txt'
+from pathlib import Path
+
+file_path = Path("C:/Users/escot/OneDrive/Escritorio/Codingnomads/python-301-main/05_exceptions/integers.txt")
 
 try: 
-    with open(file_name, "r", encoding="utf-8") as file:
+    with open(file_path, "r", encoding="utf-8") as file:
         first_line = file.readline().strip()
         number = int(first_line) # it may raise ValueError
 except IOError:
-    print(f"Error: could not read file '{file_name}'. Check if the file exists and is accessible.")
+    print(f"Error: could not read file '{file_path}'. Check if the file exists and is accessible.")
 except ValueError:
-    print(f"Error: The first line in '{file_name}' is not a valid integer.")
+    print(f"Error: The first line in '{file_path}' is not a valid integer.")
 else:
     # will run only if no exception was raised
     result = number * 2
