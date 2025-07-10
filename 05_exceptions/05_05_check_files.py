ffcf70474eb081f3bc6e09738f058350d67f19c4
E-Nincy <escotonincy10@outlook.es>
2025-07-10 15:20:10 +0800

    add text

--- 05_exceptions/05_05_check_files.py
+++ 05_exceptions/05_05_check_files.py
@@ -5,3 +5,17 @@
 # only if neither of them applies.
 
 file_name = 'integers.txt'
+
+try: 
+    with open(file_name, "r", encoding="utf-8") as file:
+        first_line = file.readline().strip()
+        number = int(first_line) # it may raise ValueError
+except IOError:
+    print(f"Error: could not read file '{file_name}'. Check if the file exists and is accessible.")
+except ValueError:
+    print(f"Error: The first line in '{file_name}' is not a valid integer.")
+else:
+    # will run only if no exception was raised
+    result = number * 2
+    print(f"The number is: {number}")
+    print(f"the result of multiplying it by 2 is. {result}")
