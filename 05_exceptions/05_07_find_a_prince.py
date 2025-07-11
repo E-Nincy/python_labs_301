0043bda21cec33dad5594308c01a20679b7eba6f
E-Nincy <escotonincy10@outlook.es>
2025-07-11 17:47:53 +0800

    add solution

--- 05_exceptions/05_07_find_a_prince.py
+++ 05_exceptions/05_07_find_a_prince.py
@@ -3,3 +3,29 @@
 # like you did in the previous exercise.
 # Raise your `PrinceException()` if the first 100 characters of a book
 # contain the string "Prince".
+
+# Custom exception
+class PrinceException(Exception):
+    def __init__(self, message="The word 'Prince' was found in content. "):
+        super().__init__(message)
+
+# Read .txt files from 'books'
+import os
+
+books_folder = ("C:/Users/escot/OneDrive/Escritorio/Codingnomads/python-301-main/05_exceptions/books")
+
+
+try: 
+    for filename in os.listdir(books_folder):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(books_folder, filename)
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read(100) 
+            if "Prince" in content:
+                raise PrinceException(f"'Prince' was found in the first 100 characters of {filename}")
+            else:
+                print(f"{filename}: No 'Prince' found in the first 100 characters.")
+except PrinceException as e:
+    print(f"Custom exception raised! : {e}")
+except Exception as general_error:
+    print(f"Something went wrong: {general_error}")
