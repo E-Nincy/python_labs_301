51d602d7baf328d4ff830d303195065da6184d39
E-Nincy <escotonincy10@outlook.es>
2025-07-14 13:19:03 +0800

    add solution

--- 06_testing/06_02_mymath-tester/test_mymath.py
+++ 06_testing/06_02_mymath-tester/test_mymath.py
@@ -4,3 +4,19 @@
 # 1. Check for correct results by providing example input.
 # 2. Check that a `CustomZeroDivisionError` gets raised correctly.
 
+import unittest
+import mymath
+
+class TestMyMath(unittest.TestCase):
+
+    def test_if_generate_correct_result(self):
+        self.assertEqual(mymath.subtract_divide(12,6,2), 3)
+
+    def test_check_CustomZeroDivisionError_raised_correctly(self):
+ 
+        with self.assertRaises(mymath.CustomZeroDivsionError) as context:
+            mymath.subtract_divide(4,0,0)
+        print(context.exception)
+
+if __name__ == "__main__":
+    unittest.main()
