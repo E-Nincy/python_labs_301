c1b32329ee2aa514fd6ce91cefcee2f4632edb44
E-Nincy <escotonincy10@outlook.es>
2025-07-15 15:56:59 +0800

    add solution

--- 07_decorators/07_05_decorated_logging.py
+++ 07_decorators/07_05_decorated_logging.py
@@ -1,3 +1,22 @@
 # Create a custom decorator function that records the execution time of
 # the decorated function and prints the time to your console when the function
 # has finished execution.
+
+import time
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()     # start Stopwatch
+        result = func(*args, **kwargs)
+        end = time.time()       # stop stopwatch
+        execution_time = end - start
+        print(f"Execution time: {execution_time:.4f} seconds")
+        return result
+    return wrapper
+
+@measure_time
+def slow_function():
+    time.sleep(2)
+    print("Done sleeping")
+
+slow_function()
