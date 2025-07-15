28fdf4e6159226f5990a0a10732918400a67449f
E-Nincy <escotonincy10@outlook.es>
2025-07-15 15:31:13 +0800

    add text

--- 07_decorators/07_02_censorship.py
+++ 07_decorators/07_02_censorship.py
@@ -4,3 +4,20 @@
 #    "I bumped my toe! Shoot!"
 # Would, after decorating it with `@censor()`, return:
 #    "I bumped my toe! S****!"
+
+def censor(func):
+    def wrapper(*args, **kwargs):
+        text = func(*args, **kwargs)
+        offensive_words = ["shoot", "darn", "heck", "fuck", "shit", "damm"]
+        for word in offensive_words:
+            censored = word[0] + "*" * (len(word) - 1)
+            text = text.replace(word, censored)
+            text = text.replace(word.capitalize(), censored)
+        return text
+    return wrapper
+
+@censor
+def say_something():
+    return "Fuck, i bumped my toe! Shoot! That hurt like heck!"
+
+print(say_something())
